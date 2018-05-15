80421b1a323fe7d5ae7d081cd8cb6a53e26fb5a6
Matt Pfeffer <matt@orangedigital.com.au>
2018-05-15 18:31:52 +1000

    Remove plugin checks
    Conventional plugins load after must use plugins so cheking for their existence will always fail.

--- wp-essentials.php
+++ wp-essentials.php
@@ -30,7 +30,6 @@ add_filter( 'the_generator', function () {
 	return '';
 });
 
-
 /**
  *  THIRD PARTY PLUGINS
  *
@@ -44,64 +43,56 @@ add_filter( 'the_generator', function () {
  * Advanced Custom Fields
  */
 
-if ( class_exists( 'acf' ) ) :
+add_filter( 'acf/settings/save_json', function ( $path ) {
+	return WP_CONTENT_DIR . '/fields';
+});
 
-	add_filter( 'acf/settings/save_json', function ( $path ) {
-		return WP_CONTENT_DIR . '/fields';
-	});
-
-	add_filter( 'acf/settings/load_json', function ( $path ) {
-		$paths[] = WP_CONTENT_DIR . '/fields';
-		return $paths;
-	});
-
-endif;
+add_filter( 'acf/settings/load_json', function ( $path ) {
+	$paths[] = WP_CONTENT_DIR . '/fields';
+	return $paths;
+});
 
 /**
  * Disable autocomplete on Gravity Forms fields. Requires
  * HTML5 output to be enabled
  */
 
-if ( class_exists( 'GFForms' ) ) :
+/**
+ * Disable autocomplete on form elements.
+ *
+ * @param string $form_tag The string containing the <form> tag.
+ * @return string The filtered string.
+ */
+function gform_tag_no_autocomplete( $form_tag ) {
 
-	/**
-	 * Disable autocomplete on form elements.
-	 *
-	 * @param string $form_tag The string containing the <form> tag.
-	 * @return string The filtered string.
-	 */
-	function gform_tag_no_autocomplete( $form_tag ) {
-
-		if ( is_admin() ) {
-			return $form_tag;
-		} else if ( GFFormsModel::is_html5_enabled() ) {
-			$form_tag = str_replace( '>', ' autocomplete="off">', $form_tag );
-		}
-
+	if ( is_admin() ) {
 		return $form_tag;
-
+	} else if ( GFFormsModel::is_html5_enabled() ) {
+		$form_tag = str_replace( '>', ' autocomplete="off">', $form_tag );
 	}
 
-	add_filter( 'gform_form_tag', 'gform_tag_no_autocomplete' );
+	return $form_tag;
 
-	/**
-	 * Disable autocomplete on input elements (input, textarea).
-	 *
-	 * @param string $field_content The field content to be filtered.
-	 * @return string The filtered field content.
-	 */
-	function gform_field_no_autocomplete( $field_content ) {
+}
 
-		if ( is_admin() ) {
-			return $field_content;
-		} else if ( GFFormsModel::is_html5_enabled() ) {
-			$field_content = preg_replace( '/<(input|textarea)/', '<${1} autocomplete="off" ', $field_content );
-		}
+add_filter( 'gform_form_tag', 'gform_tag_no_autocomplete' );
 
+/**
+ * Disable autocomplete on input elements (input, textarea).
+ *
+ * @param string $field_content The field content to be filtered.
+ * @return string The filtered field content.
+ */
+function gform_field_no_autocomplete( $field_content ) {
+
+	if ( is_admin() ) {
 		return $field_content;
-
+	} else if ( GFFormsModel::is_html5_enabled() ) {
+		$field_content = preg_replace( '/<(input|textarea)/', '<${1} autocomplete="off" ', $field_content );
 	}
 
-	add_filter( 'gform_field_content', 'gform_field_no_autocomplete' );
+	return $field_content;
 
-endif;
+}
+
+add_filter( 'gform_field_content', 'gform_field_no_autocomplete' );
